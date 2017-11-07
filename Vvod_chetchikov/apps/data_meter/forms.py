5082a6dd57b05694319b7c7d72dfce33e838ab18
magestus <magestus@yandex.ru>
2017-11-08 01:43:01 +0500

    1

--- Vvod_chetchikov/apps/data_meter/forms.py
+++ Vvod_chetchikov/apps/data_meter/forms.py
@@ -3,7 +3,7 @@ from Vvod_chetchikov.apps.data_meter.models import Data_meter
 from Vvod_chetchikov.apps.Meter.models import Meter
 
 class data_meterform(forms.ModelForm):
-    #type_meter = forms.ModelChoiceField(queryset=Meter.objects.all())
+    #type_meter = forms.ModelChoiceField(queryset=Meter.objects.all)
     class Meta:
         model = Data_meter
         fields = ['room_number', 'accounting_month', 'accounting_year', 'type_meter', 'dannie']
--- Vvod_chetchikov/apps/data_meter/views.py
+++ Vvod_chetchikov/apps/data_meter/views.py
@@ -4,21 +4,20 @@ from Vvod_chetchikov.apps.data_meter.models import Data_meter
 from Vvod_chetchikov.apps.Meter.models import Meter
 from Vvod_chetchikov.apps.data_meter.forms import data_meterform
 
+
 # Create your views here.
 
-def index (request):
-
-
+def index(request):
     meters = Meter.objects.all()
 
     form = data_meterform(initial={
-        "type_meter": meters.type_meter_id
-    })
+        "type_meter": Meter.objects
+        })
+
     context = {
         'meters': meters,
         'form': form
     }
     return render(request, 'index.html', context)
 
-
-#def data__meter(self, index):
+# def data__meter(self, index):
